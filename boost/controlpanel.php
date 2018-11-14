36764217e691c7b69ad5af4b517b40f924325b42
Matt McNaney <mcnaneym@appstate.edu>
2018-11-14 13:57:56 -0500

    Removed comment

--- boost/controlpanel.php
+++ boost/controlpanel.php
@@ -2,8 +2,6 @@
   /**
    * controlpanel.php
    *
-   * TODO: Create an icon for PLM. Don't just use teh stupid tango one.
-   *
    * @author Robert Bost <bostrt at tux dot appstate dot edu>
    */
 $link[] = array('label'       => _('Nomination Module'),
